dd973d93633cbc0bc3ccf4d43f751db1239f8d54
byungKHee <byungheekang@g.skku.edu>
2026-02-12 16:26:38 +0900

    feat: LottoReceipt 도메인 객체 구현

---
+++ src/main/kotlin/domain/LottoReceipt.kt
@@ -0,0 +1,16 @@
+package org.example.domain
+
+class LottoReceipt (val money : Money, val autoCount : Int, val manualCount : Int) {
+    init {
+        require(autoCount >= 0) { "autoCount must be > 0" }
+
+        require(manualCount >= 0) { "manualCount must be >= 0" }
+
+        require(money.amount == (autoCount + manualCount) * LOTTO_PRICE.amount) {
+            "구입 금액과 로또 개수가 일치하지 않습니다."
+        }
+    }
+    companion object {
+        val LOTTO_PRICE = Money(1000)
+    }
+}
---
+++ src/test/kotlin/domain/LottoReceiptTest.kt
@@ -0,0 +1,79 @@
+package domain
+
+import org.example.domain.LottoReceipt
+import org.example.domain.Money
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import kotlin.test.assertFailsWith
+
+class LottoReceiptTest {
+
+    @Test
+    fun issue_valid_receipt() {
+        val totalPrice : Money = Money(10000)
+        val autoCount : Int = 2
+        val manualCount : Int = 8
+
+        assertDoesNotThrow {
+            val lottoReceipt : LottoReceipt = LottoReceipt(totalPrice, autoCount, manualCount)
+        }
+    }
+
+    @Test
+    fun issue_valid_receipt_with_auto_tickets_only() {
+        val totalPrice : Money = Money(5000)
+        val autoCount : Int = 5
+        val manualCount : Int = 0
+
+        assertDoesNotThrow {
+            val lottoReceipt : LottoReceipt = LottoReceipt(totalPrice, autoCount, manualCount)
+        }
+    }
+
+    @Test
+    fun issue_valid_receipt_with_manual_tickets_only() {
+        val totalPrice : Money = Money(2000)
+        val autoCount : Int = 0
+        val manualCount : Int = 2
+
+        assertDoesNotThrow {
+            val lottoReceipt : LottoReceipt = LottoReceipt(totalPrice, autoCount, manualCount)
+        }
+    }
+
+    @Test
+    fun issue_invalid_receipt_with_mismatched_price() {
+        val totalPrice : Money = Money(3000)
+        val autoCount : Int = 2
+        val manualCount : Int = 2
+
+        assertFailsWith<IllegalArgumentException> {
+            val lottoReceipt : LottoReceipt = LottoReceipt(totalPrice, autoCount, manualCount)
+        }
+    }
+
+    @Test
+    fun issue_invalid_receipt_with_negative_ticket_count() {
+        val totalPrice : Money = Money(5000)
+        val autoCount : Int = -1
+        val manualCount : Int = 6
+
+        assertFailsWith<IllegalArgumentException> {
+            val lottoReceipt : LottoReceipt = LottoReceipt(totalPrice, autoCount, manualCount)
+        }
+    }
+
+    @Test
+    @DisplayName("총 금액보다 적은 개수의 티켓으로 영수증을 발급할 수 없다.")
+    fun issue_invalid_receipt_with_insufficient_tickets() {
+        val totalPrice : Money = Money(6000)
+        val autoCount : Int = 3
+        val manualCount : Int = 2
+
+        kotlin.test.assertFailsWith<IllegalArgumentException> {
+            val lottoReceipt : LottoReceipt = LottoReceipt(totalPrice, autoCount, manualCount)
+        }
+    }
+
+}
